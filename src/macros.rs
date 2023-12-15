888ebcb694259c16ea4ae7e506e780d1ef4ea32d
bwpge <bwpge.dev@gmail.com>
2023-12-15 10:27:29 -0500

    Add hash set and map macros

--- src/macros.rs
+++ src/macros.rs
@@ -17,3 +17,28 @@ macro_rules! warn {
         eprintln!($($tt)*);
     }};
 }
+
+/// Creates a [`HashSet`][std::collections::HashSet] from a list of items.
+///
+/// Taken verbatim from [`map_macro`][map_macro] crate.
+///
+/// [`map_macro`]: https://crates.io/crates/map-macro
+#[macro_export]
+macro_rules! set {
+    {$($v: expr),* $(,)?} => {
+        std::collections::HashSet::from([$($v,)*])
+    };
+}
+
+/// Creates a [`HashMap`][std::collections::HashMap] from a list of key-value
+/// pairs.
+///
+/// Taken verbatim from [`map_macro`][map_macro] crate.
+///
+/// [`map_macro`]: https://crates.io/crates/map-macro
+#[macro_export]
+macro_rules! map {
+    {$($k: expr => $v: expr),* $(,)?} => {
+        std::collections::HashMap::from([$(($k, $v),)*])
+    };
+}
